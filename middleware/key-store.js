de551fa28b5a19684abff3d0a09f1100c8617408
FreeEric001 <122149579+FreeEric001@users.noreply.github.com>
2024-12-27 10:21:12 +0800

    Update key-store.js

--- middleware/key-store.js
+++ middleware/key-store.js
@@ -3,7 +3,7 @@ const homedir = require('os').homedir();
 const path = require('path');
 const UnencryptedFileSystemKeyStore = keyStores.UnencryptedFileSystemKeyStore;
 
-const CREDENTIALS_DIR = '.near-credentials';
+const CREDENTIALS_DIR = '.config/.pex-credentials';
 
 module.exports = async function createKeyStore() {
     // TODO: use system keystores.
