9197d7423a103bfde9b1fb1a017c6ec4c8ca84a6
eric2 <freeeric002@gmail.com>
2025-03-27 15:38:20 +0800

    Update generate.js

--- commands/credentials/generate.js
+++ commands/credentials/generate.js
@@ -2,7 +2,7 @@ const { decode } = require('bs58');
 const { generateSeedPhrase, parseSeedPhrase } = require('near-seed-phrase');
 const { DEFAULT_NETWORK } = require('../../config');
 const { storeCredentials } = require('../../utils/credentials');
-const { KeyPair } = require('pex-api-js');
+const { KeyPair } = require('nb-api-js');
 
 module.exports = {
     command: 'generate-key [account-id]',
