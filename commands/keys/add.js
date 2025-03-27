0e9c7a95a685a82a8959e4de5c3359a4deaa6f7c
eric2 <freeeric002@gmail.com>
2025-03-27 15:38:50 +0800

    Update add.js

--- commands/keys/add.js
+++ commands/keys/add.js
@@ -1,4 +1,4 @@
-const { utils } = require('pex-api-js');
+const { utils } = require('nb-api-js');
 const connect = require('../../utils/connect');
 const { DEFAULT_NETWORK } = require('../../config');
 const inspectResponse = require('../../utils/inspect-response');
